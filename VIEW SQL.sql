58a91336edad0a5673f04e5a90d3997007f20dd1
risalsalu <risalsalu15@gmail.com>
2025-09-12 12:09:02 +0530

    sql

--- VIEW SQL.sql
+++ VIEW SQL.sql
@@ -1,1 +1,19 @@
-SELECT * FROM Authors
+DROP VIEW IF EXISTS BookAuthorDetail
+GO
+
+CREATE VIEW BookAuthorDetail
+AS 
+SELECT
+b.BookID,
+b.Title AS BookTitles,
+b.PublisherID,
+a.AuthorID,
+a.AuthorName,
+a.BirthYear
+
+
+FROM Books b
+INNER JOIN Authors a ON b.AuthorID = a.AuthorID
+GO 
+
+SELECT * FROM BookAuthorDetail;
